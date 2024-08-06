506cd03423d193a298ad03518fc8422c4af1081c
Bogdan <bogdancostinvlad@gmail.com>
2024-08-06 13:25:22 +0100

    FINAL - push before complete redesign

--- factory.py
+++ factory.py
@@ -14,9 +14,9 @@ class SimulationFactory:
         colonies = [
             Colony(random.randint(50, config.screen_width - 50), random.randint(50, config.screen_height - 50), (255, 0, 0),
                    config, pheromone_manager, food_manager),
-            Colony(random.randint(50, config.screen_width - 50), random.randint(50, config.screen_height - 50), (0, 0, 255),
-                   config, pheromone_manager, food_manager),
-            Colony(random.randint(50, config.screen_width - 50), random.randint(50, config.screen_height - 50), (0, 0, 0),
-                   config, pheromone_manager, food_manager)
+            #Colony(random.randint(50, config.screen_width - 50), random.randint(50, config.screen_height - 50), (0, 0, 255),
+             #      config, pheromone_manager, food_manager),
+            #Colony(random.randint(50, config.screen_width - 50), random.randint(50, config.screen_height - 50), (0, 0, 0),
+             #      config, pheromone_manager, food_manager)
         ]
         return screen, pheromone_manager, food_manager, colonies
--- simulation.py
+++ simulation.py
@@ -2,6 +2,7 @@ import pygame
 import random
 import math
 from collections import defaultdict
+from scipy.stats import norm
 
 # Colors
 WHITE = (255, 255, 255)
@@ -28,10 +29,10 @@ class Ant:
         self.color = color
         self.food = False
         self.angle = random.uniform(0, 2 * math.pi)
-        self.speed = 2
+        self.speed = args.ant_speed
         self.pheromone_manager = pheromone_manager
         self.lifetime = random.uniform(500, lifetime)
-        self.viewRange = 50
+        self.viewRange = 20
         self.pheromone_detection_threshold = args.pheromone_cull_threshold
         self.foodCarry = 0
         self.biteSize = 10  # how much food can pick up at one time
@@ -47,14 +48,13 @@ class Ant:
 
         # logging
         self.stats = {
-            'distanceTravelled' : 0
+            'distanceTravelled': 0
         }
 
     def move(self):
         # Basic movement
         self.x += math.cos(self.angle) * self.speed
         self.y += math.sin(self.angle) * self.speed
-
 
         # Boundary checking
         if self.x < 0 or self.x > self.args.screen_width or self.y < 0 or self.y > self.args.screen_height:
@@ -64,7 +64,11 @@ class Ant:
         self.stats['distanceTravelled'] += self.speed
 
     def draw(self, screen):
-        pygame.draw.circle(screen, self.color, (int(self.x), int(self.y)), 2)
+        col = self.color
+        if self.food:
+            col = GREEN
+
+        pygame.draw.circle(screen, col, (int(self.x), int(self.y)), 2)
 
     def detectPheromones(self):
         grid_x, grid_y = int(self.x / self.args.grid_size), int(self.y / self.args.grid_size)
@@ -87,7 +91,7 @@ class Ant:
         return best_tile
 
     def randomWalk(self):
-        self.speed = 2
+        self.speed = self.args.ant_speed
 
         # update internal state
         self.impatience += 1
@@ -97,8 +101,11 @@ class Ant:
             self.angle = random.uniform(self.angle - math.pi / 2, self.angle + math.pi / 2)
             self.impatience = 0
 
-    def walkToTarget(self, x, y):
+    def walkToTarget(self, x, y, noise=None):
+        self.speed = self.args.ant_speed
         self.angle = math.atan2(y - self.y, x - self.x)
+        if noise:
+            self.angle += noise
 
     def checkIfHomeBehaviour(self):
         if distance(self.x, self.y, self.home['x'], self.home['y']) < 5:
@@ -109,7 +116,19 @@ class Ant:
             self.layPheromone = False
 
     def carriesFoodBehaviour(self):
-        self.walkToTarget(self.home['x'], self.home['y'])
+        noise = random.normalvariate(0, math.pi / 2) # have a bit of noise each time unless close to home
+        if random.randint(0, 10) < 3: # once in a while bias towards pheromone trail
+            detected_pheromone_tile = self.detectPheromones()
+            if detected_pheromone_tile:
+                target_x, target_y = detected_pheromone_tile
+                pheromone_angle = math.atan2(target_y - self.y, target_x - self.x)
+                noise = random.normalvariate(abs(pheromone_angle-self.angle), math.pi / 8)
+
+        if is_in_cone(self.x, self.y, self.home['x'], self.home['y'], self.angle, self.viewRange):
+            self.walkToTarget(self.home['x'], self.home['y'])
+        else:
+            self.walkToTarget(self.home['x'], self.home['y'], noise)
+
         self.layPheromone = True
         self.checkIfHomeBehaviour()
 
@@ -120,7 +139,6 @@ class Ant:
             self.walkToTarget(target_x, target_y)
 
     def brain(self):
-        detected_pheromone_tile = self.detectPheromones()
         nearby_food = self.food_manager.check_for_food(self.x, self.y, self.angle, self.viewRange)
 
         if nearby_food and not self.food:
@@ -129,9 +147,10 @@ class Ant:
                 self.food = True
                 self.foodCarry = self.biteSize
                 nearby_food.pickUp(self.biteSize)
+                self.angle += math.pi
         elif self.foodCarry > 0:
             self.carriesFoodBehaviour()
-        elif random.randint(0, 10) <= 8:
+        elif random.randint(0, 10) <= 7:
             self.sniffPheromonesBehaviour()
         else:
             self.randomWalk()
@@ -203,7 +222,6 @@ class Colony:
         if self.cycles_to_spawn <= 0:
             self.cycles_to_spawn = self.args.cycles_to_spawn
             self.spawnAnts(self.args.ants_spawn_number)
-
 
     def draw(self, screen):
         pygame.draw.circle(screen, self.color, (int(self.x), int(self.y)), 10)
@@ -280,10 +298,10 @@ class FoodManager:
             self._remove_from_grid(pile)
 
 
-def is_in_cone(x1, y1, x2, y2, direction, max_range, angle=math.pi / 4):
+def is_in_cone(x1, y1, x2, y2, direction, max_range, angle=math.pi / 4, min_range = 1):
     dx, dy = x2 - x1, y2 - y1
     dist = math.sqrt(dx ** 2 + dy ** 2)
-    if dist > max_range:
+    if dist > max_range or dist < min_range:
         return False
     angle_to_target = math.atan2(dy, dx)
     delta_angle = abs(direction - angle_to_target)
@@ -332,8 +350,7 @@ class PheromoneManager:
                     continue
                 new_pheromones[(grid_x, grid_y)] = strength
                 # Diffuse pheromones to neighbors
-                diffusion_amount = strength * self.args.pheromone_diffusion_rate * (
-                        strength / self.args.pheromone_saturation) / 8
+                diffusion_amount = strength * self.args.pheromone_diffusion_rate / 8
                 if diffusion_amount > self.args.pheromone_cull_threshold:
                     new_pheromones[(grid_x, grid_y)] -= diffusion_amount * 8
                     for dx, dy in [(-1, 0), (1, 0), (0, -1), (0, 1), (-1, -1), (1, 1), (-1, 1), (1, -1)]:
@@ -361,7 +378,7 @@ class PheromoneManager:
                      (grid_x, grid_y) in self.pheromones[color]}
         return strengths
 
-    def get_nearest_pheromone_tile(self, color, x, y, min_strength, max_range, direction=None):
+"""    def get_nearest_pheromone_tile(self, color, x, y, min_strength, max_range, direction=None):
         nearest_tile = None
         nearest_distance = max_range
         grid_x, grid_y = int(x / self.args.grid_size), int(y / self.args.grid_size)
@@ -377,3 +394,4 @@ class PheromoneManager:
                                         py * self.args.grid_size + self.args.grid_size // 2)
 
         return nearest_tile
+"""
